fffdbf3af64f6880c5044033e9b837b1fe4f3347
DAVIDPABON173 <davidpabon173@gmail.com>
2018-04-09 17:25:44 -0500

    Eliminación de imports innecesarios

--- src/patterns/main/Application.java
+++ src/patterns/main/Application.java
@@ -2,24 +2,10 @@ package patterns.main;
 
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-
-import patterns.bussiness.iterator.Iterator;
-import patterns.bussiness.motors.MotorAdapterDiesel;
-import patterns.bussiness.motors.MotorDiesel;
-import patterns.bussiness.motors.interfaces.IMotor;
-import patterns.bussiness.orders.InternationalOrder;
-import patterns.bussiness.orders.NationalOrder;
 import patterns.bussiness.pattern.facade.FacadePattern;
-import patterns.bussiness.pattern.factory.Factory;
-import patterns.bussiness.scenarios.Escenario;
-import patterns.bussiness.strategies.Estrategia;
-import patterns.bussiness.strategies.EstrategiaA;
-import patterns.bussiness.strategies.EstrategiaB;
-import patterns.bussiness.tests.Prueba;
 import patterns.bussiness.vehicles.Vehiculo;
-import patterns.bussiness.vehicles.VehiculoDeportivo;
+
 
 public class Application {
 
